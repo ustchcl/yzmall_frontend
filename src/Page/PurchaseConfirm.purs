887863e6b2cc437de9f221ff1360b8abbe507e8d
黄成林 <huangchenglin@huangchenglindeMacBook-Pro.local>
2019-03-14 10:50:44 +0800

    purchase confirm

--- src/Page/PurchaseConfirm.purs
+++ src/Page/PurchaseConfirm.purs
@@ -3,18 +3,25 @@ module Yzmall.Page.PurchaseConfirm where
 import Halogen.Themes.Bootstrap4
 import Prelude
 
+import Affjax.RequestBody (RequestBody(..))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Network.RemoteData (RemoteData)
 import Slug (Slug)
 import Yzmall.Data.Account (Account)
+import Yzmall.Data.Commodity (Commodity)
 import Yzmall.Utils (cls, style, (<+>), (->>))
 
 type Input = 
   { slug :: Slug
   }
 
-type State = Int
+type State = 
+  { account :: Maybe Account
+  , address :: RemoteData String Address 
+  , commodity :: RemoteData String Commodity
+  }
